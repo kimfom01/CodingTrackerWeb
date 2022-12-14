4e0c953fc4bb8a443f8cfd6a72f85b10c36dc193
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-12-14 10:24:46 +0300

    Add Ef DataAccess class

---
+++ Data/EntityFrameworkDataAccess.cs
@@ -0,0 +1,31 @@
+using CodingTrackerWeb.Models;
+
+namespace CodingTrackerWeb.Data;
+
+public class EntityFrameworkDataAccess : IDataAccess
+{
+    public void InsertRecord(CodingHours codingHours)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteRecord(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<CodingHours> GetAllRecords()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateRecord(int id, CodingHours codingHours)
+    {
+        throw new NotImplementedException();
+    }
+
+    public CodingHours GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
