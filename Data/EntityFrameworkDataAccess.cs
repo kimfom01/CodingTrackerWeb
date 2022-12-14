3e5870ce2ed468bd4f624ef25aa8a572fb667b25
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-12-14 10:35:46 +0300

    Implement EF DataAccess methods

--- Data/EntityFrameworkDataAccess.cs
+++ Data/EntityFrameworkDataAccess.cs
@@ -1,31 +1,54 @@
+using System.Globalization;
+using CodingTrackerWeb.Context;
 using CodingTrackerWeb.Models;
 
 namespace CodingTrackerWeb.Data;
 
 public class EntityFrameworkDataAccess : IDataAccess
 {
+    private readonly CodingHoursContext _db;
+
+    public EntityFrameworkDataAccess(CodingHoursContext db)
+    {
+        _db = db;
+    }
+
     public void InsertRecord(CodingHours codingHours)
     {
-        throw new NotImplementedException();
+        _db.Add(codingHours);
+        _db.SaveChanges();
     }
 
     public void DeleteRecord(int id)
     {
-        throw new NotImplementedException();
+        var record = GetById(id);
+        _db.Remove(record);
+        _db.SaveChanges();
     }
 
     public List<CodingHours> GetAllRecords()
     {
-        throw new NotImplementedException();
+        return _db.CodingHours.ToList();
     }
 
     public void UpdateRecord(int id, CodingHours codingHours)
     {
-        throw new NotImplementedException();
+        var record = GetById(id);
+        record.StartTime = codingHours.StartTime;
+        record.EndTime = codingHours.EndTime;
+        record.Duration = GetDuration(codingHours.StartTime, codingHours.EndTime);
     }
 
     public CodingHours GetById(int id)
     {
-        throw new NotImplementedException();
+        return _db.CodingHours.First(x => x.Id == id);
+    }
+    
+    private string GetDuration(string startTime, string endTime)
+    {
+        DateTime parsedStartTime = DateTime.ParseExact(startTime, "HH:mm", null, DateTimeStyles.None);
+        DateTime parsedEndTime = DateTime.ParseExact(endTime, "HH:mm", null, DateTimeStyles.None);
+
+        return parsedEndTime.Subtract(parsedStartTime).ToString();
     }
 }
