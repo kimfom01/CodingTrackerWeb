b71fe80697d2bb604f3aea2dc7cf9b0769a91b7d
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-01-12 15:59:18 +0300

    Update model structure to page level namespace

--- Models/CodingHour.cs
+++ Models/CodingHour.cs
@@ -1,21 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace CodingTrackerWeb.Models
+namespace CodingTrackerWeb.Models;
+
+public class CodingHour
 {
-    public class CodingHour
-    {
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
-        public string Date { get; set; } = string.Empty;
+    [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+    public string Date { get; set; } = string.Empty;
 
-        [DisplayFormat(DataFormatString = "{0:HH:mm}", ApplyFormatInEditMode = true)]
-        public string StartTime { get; set; } = string.Empty;
+    [DisplayFormat(DataFormatString = "{0:HH:mm}", ApplyFormatInEditMode = true)]
+    public string StartTime { get; set; } = string.Empty;
 
-        [DisplayFormat(DataFormatString = "{0:HH:mm}", ApplyFormatInEditMode = true)]
-        public string EndTime { get; set; } = string.Empty;
+    [DisplayFormat(DataFormatString = "{0:HH:mm}", ApplyFormatInEditMode = true)]
+    public string EndTime { get; set; } = string.Empty;
 
-        [DisplayFormat(DataFormatString = "{0:HH:mm}", ApplyFormatInEditMode = true)]
-        public string Duration { get; internal set; } = string.Empty;
-    }
+    [DisplayFormat(DataFormatString = "{0:HH:mm}", ApplyFormatInEditMode = true)]
+    public string Duration { get; internal set; } = string.Empty;
 }
