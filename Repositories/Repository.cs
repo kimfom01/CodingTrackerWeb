45484b5a6244e8656882dcd200b0366e229a6281
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-04-03 15:59:03 +0300

    Make db set to protected for inheritance access

--- Repositories/Repository.cs
+++ Repositories/Repository.cs
@@ -7,33 +7,33 @@ namespace CodingTrackerWeb.Repositories;
 public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : class
 {
     private readonly CodingTrackerWebContext _dbContext;
-    private readonly DbSet<TEntity> _dbSet;
+    protected readonly DbSet<TEntity> DbSet;
 
     public Repository(CodingTrackerWebContext dbContext)
     {
         _dbContext = dbContext;
-        _dbSet = dbContext.Set<TEntity>();
+        DbSet = dbContext.Set<TEntity>();
     }
 
     public virtual void DeleteRecord(int id)
     {
         TEntity? entity = GetById(id);
-        _dbSet.Remove(entity);
+        DbSet.Remove(entity);
     }
 
     public virtual List<TEntity?> GetUserRecords(Expression<Func<TEntity?, bool>> predicate)
     {
-        return _dbSet.Where(predicate).AsNoTracking().ToList();
+        return DbSet.Where(predicate).AsNoTracking().ToList();
     }
 
     public virtual TEntity? GetById(int id)
     {
-        return _dbSet.Find(id);
+        return DbSet.Find(id);
     }
 
     public virtual void InsertRecord(TEntity? entity)
     {
-        _dbSet.Add(entity);
+        DbSet.Add(entity);
     }
 
     public virtual void UpdateRecord(int id, TEntity entity)
